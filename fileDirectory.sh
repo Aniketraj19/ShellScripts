7012e7bb28a2fa5a31b02a35f2d96747b3196b49
Aniket Raj <aniketraj19verma@gmail.com>
2023-09-29 19:01:57 +0530

    task1-complete

--- fileDirectory.sh
+++ fileDirectory.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # File and Directory Operations:
 # Write a shell script that takes a directory path as input and performs the following operations:
 
@@ -5,4 +7,21 @@
 # Count the number of files and directories.
 # Calculate the total size of all files in the directory.
 
+function memory_usage() {
+    mem=0
+    for item in $1
+    do
+        size=$(stat -c %s "$item")
+        mem=$((mem + size))
+    done
+    echo "The total size of the files in the directory is: $mem bytes"
+}
 
+
+echo "The directory $1 is being used: "
+ls -l $1 
+
+ls -l $1 | wc -l
+
+ls -l $1 | awk '{print $5}' > /home/arv/memory
+memory_usage $1
